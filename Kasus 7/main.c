c54d85b365081161693536e6f687f72afea009a0
auliaRachmad <aulia.rachmad21@gmail.com>
2025-05-09 04:14:57 +0700

    Add files via upload

---
+++ Kasus 7/main.c
@@ -0,0 +1,22 @@
+#include "nbtrees.h"
+
+int main(){
+	//Untuk sample tree akan menggunakan yang ada pada dokumentasi penugasan
+	Isi_Tree tree;
+	
+	Create_tree(tree, 10);
+	
+	PreOrder(tree);
+	InOrder(tree);
+	PostOrder(tree);
+	Level_order(tree, 10);
+	
+	PrintTree(tree);
+	
+	printf("\nJumlah elemen : %d", nbElmt(tree));
+	printf("\nJumlah daun : %d", nbDaun(tree));
+	
+	printf("\nLevel pada info 'C' : %d", Level(tree, 'C'));
+	
+	printf("\nKedalaman pohon : %d", Depth(tree));
+}

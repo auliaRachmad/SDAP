6fe5a5fe96084bdf9bdf88931ae33ffd34fa1ca2
auliaRachmad <aulia.rachmad21@gmail.com>
2025-05-23 07:11:20 +0700

    Add files via upload

--- Kasus 7/main.c
+++ Kasus 7/main.c
@@ -4,12 +4,12 @@ int main(){
 	//Untuk sample tree akan menggunakan yang ada pada dokumentasi penugasan
 	Isi_Tree tree;
 	
-	Create_tree(tree, 10);
+	Create_tree(tree, 5);
 	
 	PreOrder(tree);
 	InOrder(tree);
 	PostOrder(tree);
-	Level_order(tree, 10);
+	Level_order(tree, 5);
 	
 	PrintTree(tree);
 	
